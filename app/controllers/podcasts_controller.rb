6ae107b0597a978355becdd84a357a40cd432d00
Michael Burak <michaelrburak@gmail.com>
2017-01-27 23:25:28 +0000

    non-logged in users can not see podcasts or create them

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,12 +1,18 @@
 class PodcastsController < ApplicationController
 
   get '/podcasts' do
+    if logged_in?
     @podcasts = Podcast.all
     erb :'podcasts/index'
+  else redirect '/signup'
   end
+end
 
   get '/podcasts/new' do
+    if logged_in?
     erb :'podcasts/create_podcast'
+  else redirect '/signup'
   end
+end
 
 end
