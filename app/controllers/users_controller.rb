625db5d4faf206378a9085d0f9da231b1bb4c115
Michael Burak <michaelrburak@gmail.com>
2017-01-27 22:51:44 +0000

    added login routes, login page, logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,32 @@ end
     @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
     if @user.save
       session[:user_id] = @user.id
-        redirect '/'
+        redirect '/podcasts'
     else redirect '/signup'
       end
     end
+
+    get '/login' do
+      if logged_in? then
+       redirect '/podcasts'
+     else
+      erb :'users/login'
+    end
+  end
+
+    post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect '/podcasts'
+        else
+            redirect '/login'
+        end
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/login"
+  end
 
 end
