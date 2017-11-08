3aa8c41ec39c37862cc910ea06116f6ea8cf1d29
Michael Burak <michaelrburak@gmail.com>
2017-11-08 16:33:49 -0500

    Attempting to resolve database transition

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,19 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do
+ set :database, 'sqlite:///dev.db'
+ set :show_exceptions, true
+end
+
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+ end
 
 require_all 'app'
